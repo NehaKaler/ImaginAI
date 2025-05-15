6a0e43bdd48097a417384c227dbfc8e90c6de6ee
Neha Kaler <163890967+NehaKaler@users.noreply.github.com>
2025-05-15 22:51:00 +0530

    Update CreatePost.jsx

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -6,8 +6,8 @@ import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const [form, setForm] = useState({
     name: '',
     prompt: '',
@@ -17,140 +17,148 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
-  const handleSurpriseMe = () => {
-    const randomPrompt = getRandomPrompt(form.prompt);
-    setForm({ ...form, prompt: randomPrompt });
-  };
-
   const generateImage = async () => {
-    if (form.prompt) {
+    if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+        const response = await fetch('https://min-journey-dall-e.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        });
+          body: JSON.stringify({ prompt: form.prompt }),
+        })
 
         const data = await response.json();
-        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-      } catch (err) {
-        alert(err);
+
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert(error);
       } finally {
         setGeneratingImg(false);
       }
     } else {
-      alert('Please provide proper prompt');
+      alert('Please enter a valid prompt')
     }
-  };
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (form.prompt && form.photo) {
+    if(form.prompt && form.photo) {
       setLoading(true);
+
       try {
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        const response = await fetch('https://min-journey-dall-e.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ ...form }),
+          body: JSON.stringify({...form}),
         });
 
         await response.json();
         alert('Success');
         navigate('/');
-      } catch (err) {
-        alert(err);
+      } catch (error) {
+        alert(error)
       } finally {
         setLoading(false);
       }
     } else {
-      alert('Please generate an image with proper details');
+      alert('Please enter a prompt and generate an image!')
     }
   };
 
-  return (
-    <section className="max-w-7xl mx-auto">
-      <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
-      </div>
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
 
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5">
-          <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex., john doe"
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
+  }
+
+  return (
+    <section className='max-w-7xl mx-auto'>
+    {/* Heading and Subtitle */}
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create AI Images 
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+          Explore to test out your very own DALL-E OpenAI generated images! Share your creativity with the community!
+        </p>
+      </div>
+    {/* Name and Prompt FormFields */}
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            labelName='Your Name'
+            type='text'
+            name='name'
+            placeholder='Aung San Suu Kyi'
             value={form.name}
             handleChange={handleChange}
           />
-
-          <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+          <FormField 
+            labelName='Prompt'
+            type='text'
+            name='prompt'
+            placeholder='a surrealist dream-like oil painting by Salvador Dalí of a cat playing chess with Van Gogh in a starry night'
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
-
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
-            { form.photo ? (
-              <img
+        {/* AI Image */}
+          <div className='relative bg-gray-50 border border-red-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-xl p-3 h-xl flex justify-center items-center'>
+            {form.photo ? (
+              <img 
                 src={form.photo}
                 alt={form.prompt}
-                className="w-full h-full object-contain"
+                className='w-full h-full object-contain'
               />
             ) : (
-              <img
+              <img 
                 src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
+                alt='preview'
+                className='w-9/12 h-9/12 object-contain opacity-40'
               />
             )}
-
+            
             {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
                 <Loader />
               </div>
             )}
           </div>
         </div>
-
-        <div className="mt-5 flex gap-5">
+      {/* Submit Button */}
+        <div className='mt-5 flex gap-5'>
           <button
-            type="button"
+            type='button'
             onClick={generateImage}
-            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:scale-125 transition duration-500 ease-in-out'
           >
             {generatingImg ? 'Generating...' : 'Generate'}
           </button>
         </div>
-
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+      {/* Share Button */}
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>
+            Feel free to share your AI generated image with the community! 
+          </p>
           <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center hover:scale-125 transition duration-500 ease-in-out'
           >
-            {loading ? 'Sharing...' : 'Share with the Community'}
+            {loading ? 'Sharing...' : 'Share to Community!'}
           </button>
         </div>
       </form>
     </section>
-  );
-};
+  )
+}
 
-export default CreatePost;
+export default CreatePost
